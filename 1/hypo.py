04796ddb4b69ca5bdad35fa3cdf9d68f702a2982
Aravind1998 <aravindsuresh98@gmail.com>
2018-10-07 12:36:16 +0530

    Added First Program

---
+++ 1/hypo.py
@@ -0,0 +1,38 @@
+# Labels :- Sky,AirTemp,Humidity,Wind,Water,Forecast,Enjoy Sport
+# Labels :- Color, Size, Act, Age, Inflated
+
+
+import csv
+hypo = [0,0,0,0]
+
+with open('Test-data.csv') as csv_file :
+    readcsv = csv.reader(csv_file,delimiter = ',')
+    print(readcsv)
+    data = []
+    print("Examples are :")
+    for row in readcsv:
+        print(row)
+        if row[len(row)-1].upper() == "T":
+            data.append(row)
+print("Positive examples are :-")
+for x in data:
+    print(x)
+
+TotalExamples = len(data)
+i = 0
+print("Steps of Find-S algorithm are\n",hypo)
+list = []
+p = 0
+d = len(data[p]) -1
+for j in range(d):
+    list.append(data[i][j])
+hypo = list
+i = 1
+
+for i in range(TotalExamples):
+    for k in range(d):
+        if hypo[k] != data[i][k]:
+            hypo[k] = '?'
+        else:
+            hypo[k]
+print(hypo)
